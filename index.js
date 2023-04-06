97169a041c6dc75438c6b0a2d2fbaa848ad7165a
Abdiladi22 <abdiladifali0@gmail.com>
2023-04-07 02:38:48 +0300

    compelet assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+let rmMain = document.getElementById('main');
+rmMain.remove();
+let newHeader = document.createElement('h1');
+newHeader.setAttribute('id','victory');
+newHeader.textContent = "is the champion";
